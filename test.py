91c3442658fd2bcde7355130e94889dfc0d2d1d0
Thiago Garcia <thiagogds14@gmail.com>
2014-11-28 18:32:19 -0200

    Start project

---
+++ test.py
@@ -0,0 +1,10 @@
+import unittest
+from unittest import TestCase
+
+class VivoScrapeerTest(TestCase):
+    def test_get_login(self):
+        self.assertTrue(True)
+
+
+if __name__ == '__main__':
+    unittest.main()
