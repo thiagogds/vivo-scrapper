41deae8ff08b137910648770d1121868485b85ae
Thiago Garcia <thiagogds14@gmail.com>
2015-01-28 19:03:23 -0200

    Create runner.

---
+++ runner.py
@@ -0,0 +1,22 @@
+from scrapper import Vivo
+import mandrill
+
+def run():
+    client = Vivo()
+    client._parse()
+    availables = client._save_tickets()
+
+    mandrill_client = mandrill.Mandrill('wmwohycS5Po67rg7gK0Xmw')
+    message = {
+       'from_email': 'thiagogds14@gmail.com',
+       'from_name': 'Vivo Scrapper',
+       'subject': 'Tem evento novo!!!!',
+       'text': 'http://www.tvantagens.com.br/',
+       'to': [{'email': 'thiagogds14@gmail.com',
+            'type': 'to'}],
+    }
+
+    mandrill_client.messages.send(message=message)
+
+if __name__ == '__main__':
+    run()
