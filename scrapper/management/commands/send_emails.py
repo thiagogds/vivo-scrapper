3037c81249a915ecdb495ca8d7c13fb4cd71e383
Thiago Garcia <thiagogds14@gmail.com>
2015-07-11 16:27:46 -0300

    Create command to send emails to subscriber with new tickets.

---
+++ scrapper/management/commands/send_emails.py
@@ -0,0 +1,28 @@
+from decouple import config
+from lib.mail import EmailTemplate
+
+from django.core.management.base import BaseCommand, CommandError
+from django.contrib.auth import get_user_model
+
+from scrapper.scrapper import Vivo
+
+
+User = get_user_model()
+class Command(BaseCommand):
+    help = 'Send new available tickets email'
+
+    def handle(self, *args, **options):
+        client = Vivo()
+        client._get_ticket_info()
+        availables = client._save_tickets()
+
+        if len(availables) > 0:
+            emails = User.objects.values_list('email', flat=True)
+            mail = EmailTemplate(
+                subject=u'Tem evento novo!', bcc=emails,
+                tpl_message=u'scrapper/mail.txt',
+                tpl_alternative=u'scrapper/mail.html',
+                context={'tickets': availables}
+            )
+
+            mail.send()
---
+++ scrapper/tests/test_commands.py
@@ -0,0 +1,32 @@
+#coding: utf-8
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.core.management import call_command
+from django.core import mail
+
+from mock import patch
+from unipath import Path
+from model_mommy.mommy import make as m
+
+from scrapper.models import Ticket
+from scrapper.scrapper import Vivo
+
+User = get_user_model()
+class TestSendEmail(TestCase):
+
+    @patch('scrapper.scrapper.Vivo')
+    def test_send_email_for_users(self, mock_client):
+        instance = mock_client.return_value
+        instance._save_tickets.return_value = [m(Ticket, avaliabilty='BK'),]
+        m(User, email="john@example.com")
+        m(User, email="john+1@example.com")
+        m(User, email="john+2@example.com")
+
+        self.assertEqual(0, len(mail.outbox))
+
+        call_command('send_emails')
+
+        self.assertEqual(1, len(mail.outbox))
+        self.assertIn('john@example.com', mail.outbox[0].bcc)
+        self.assertIn('john+1@example.com', mail.outbox[0].bcc)
+        self.assertIn('john+2@example.com', mail.outbox[0].bcc)
