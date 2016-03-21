cc2308b5090e776814fbc343e28c58accc015a16
Thiago Garcia <thiagogds14@gmail.com>
2016-03-21 18:01:17 -0300

    Uses spark post lib to send mail

--- vivo_scrapper/settings.py
+++ vivo_scrapper/settings.py
@@ -29,7 +29,6 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'simple_email_confirmation',
-    'djrill',
     'registration',
     'scrapper',
 )
@@ -77,11 +76,7 @@ EMAIL_BACKEND = config('EMAIL_BACKEND',
     default='django.core.mail.backends.smtp.EmailBackend'
 )
 DEFAULT_FROM_EMAIL = "no-reply@ingressosgratis.com"
-EMAIL_HOST = 'smtp.sparkpostmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'SMTP_Injection'
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
-EMAIL_USE_TLS = True
+SPARKPOST_API_KEY = config('SPARKPOST_API_KEY')
 
 LANGUAGE_CODE = 'pt-br'
 TIME_ZONE = 'America/Sao_Paulo'
